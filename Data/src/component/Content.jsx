514b96a2e07e1bb8a329d42d13a2fb98804b7108
ajithkumar-ravi <itsajith005@gmail.com>
2025-10-07 02:42:36 -0700

    Added: Data folder

---
+++ Data/src/component/Content.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import styled from "styled-components";
+import CounterApp from "./CounterApp";
+
+let Button = styled.button`
+  background-color: blue;
+  color: white;
+  width: 100px;
+  hight: 100px;
+  padding: 10px;
+`;
+
+let NewButton = styled(Button)`
+  background-color: red;
+  box-shadow: 10px 10px 5px black;
+`;
+
+let user = "Dhanush";
+
+function printSomething(e){
+  // console.log("Hello");
+  user = "Johny";
+}
+function printSomething1(evant){
+  // console.log(user);
+  user = "Settan";
+  
+}
+
+ 
+const Content = () => {
+  return (
+    <main>
+      <h1 style={{ backgroundColor: "red", color: "white" }}>Main Contents - {user}</h1>
+      <Button onClick={printSomething}>Click Me</Button>
+      <NewButton onClick={(e)=>{printSomething1(e)}}>New btn</NewButton>
+      <CounterApp/>
+    </main>
+  );
+};
+
+export default Content;
