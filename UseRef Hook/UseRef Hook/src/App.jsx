7b741425d3269457cc1b063328b8e96fa9667013
ajithkumar-ravi <itsajith005@gmail.com>
2025-11-20 15:05:01 +0530

    UseRef completed

---
+++ UseRef Hook/UseRef Hook/src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { useEffect } from 'react';
+import { useRef } from 'react';
+import { useState } from 'react';
+
+function App() {
+
+  const [input, setInput] = useState("")
+  
+  const inputRef = useRef()
+
+  console.log("Rendered");
+
+  useEffect(() => {
+    inputRef.current = input
+  }, [input])
+
+  const displaly = () => {
+    console.log(inputRef.current.value);
+  }
+  
+  return (
+    <div>
+      <h1>Input</h1>
+      <input type="text" value={input} onChange={(event) => setInput(event.target.value)} />
+      {/* <p>My name is: {input}</p> */}
+
+      {/* <input type="text" ref={inputRef} />   */}
+      <p>My Name is : {input}</p>
+      <p>My Name is : {inputRef.current}</p>
+      <button onClick={displaly}>Add</button>
+    </div>
+  )
+}
+
+export default App
