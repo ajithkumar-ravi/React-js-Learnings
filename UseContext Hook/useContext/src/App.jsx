df6914ff0fbb68b82e7e3e8a3c6ef77baccebc24
ajithkumar-ravi <itsajith005@gmail.com>
2025-11-29 12:27:58 +0530

    UseContext Initial commit

---
+++ UseContext Hook/useContext/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Container from './Container'
+
+const App = () => {
+  return (
+    <div>
+      <h1>App Component</h1>
+      <Container/>
+    </div>
+  )
+}
+
+export default App
---
+++ UseContext Hook/useContext/src/Users.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import User from './User.jsx'
+
+const Users = () => {
+  return (
+    <div>
+        <h1>Users Compt</h1>
+        <User/>
+    </div>
+  )
+}
+
+export default Users
