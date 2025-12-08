1f5417d348c752142eea0d5493a1420cfc6f4afe
ajithkumar-ravi <itsajith005@gmail.com>
2025-12-08 15:12:30 +0530

    Created Navbar and other component

---
+++ Protected-Routes/Protected-routes/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <h1>React Nav</h1>
+        <ul>
+            <li>Login</li>
+            <li>Setting</li>
+            <li>Logout</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
